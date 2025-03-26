4b0920baea46e2a3f22d88fdf582322c42d9a747
AiGentMaster <AiGentMaster@gmail.com>
2025-03-26 13:26:06 +1000

    Add development flavor source directory

---
+++ app/src/development/java/com/example/tixelcheck/Config.java
@@ -0,0 +1,9 @@
+package com.example.tixelcheck;
+
+/**
+ * Development-specific configuration
+ */
+public class Config {
+    public static final String API_BASE_URL = "https://dev-api.tixel.com/";
+    public static final int MIN_REFRESH_INTERVAL_MINUTES = 1;
+}
