258b27122a1505b97135601cc4c19650f06f6f30
AiGentMaster <AiGentMaster@gmail.com>
2025-03-26 13:25:50 +1000

    Add simple test class to ensure test directory exists

---
+++ app/src/test/java/com/example/tixelcheck/SimpleTest.java
@@ -0,0 +1,12 @@
+package com.example.tixelcheck;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class SimpleTest {
+    @Test
+    public void emptyTest() {
+        // A simple passing test to ensure the test framework is working
+        assertTrue(true);
+    }
+}
